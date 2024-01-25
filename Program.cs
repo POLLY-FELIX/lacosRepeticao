2bc53e0ec85ed0731ca2043898ec197653712e55
Aluno <Aluno@DESKTOP-SANL2T8>
2024-01-24 21:41:47 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lacosRepeticao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int operacao = 0;
+            while (operacao != 4) 
+            {
+
+                if (operacao == 0)
+                {
+                    Console.WriteLine("Bem vindo ao menu:");
+                    Console.WriteLine("Opções");
+                    Console.WriteLine("1 - função 1");
+                    Console.WriteLine("2 - função 2");
+                    Console.WriteLine("3 - função 3");
+                    Console.WriteLine("9 - sair");
+                }
+
+                if (operacao == 0)
+                {
+                    Console.WriteLine("Bem vindo a operação 1");
+                }
+
+
+            }
+        }
+   }
+}
